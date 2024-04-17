42005a03b419f76f9e9e09a659441cda28c388b7
AdriellBezerra <adrielprof01@gmail.com>
2024-04-16 22:13:31 -0300

    Projeto criado, primeira entidade, conexão com H2

---
+++ src/main/java/br/com/carcontrol/entidades/Veiculo.java
@@ -0,0 +1,102 @@
+package br.com.carcontrol.entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Veiculo implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String fabricante;
+	private String modelo;
+	private String anoFabricacao;
+	private String placa;	
+	
+	public Veiculo() {
+		
+	}
+	
+	public Veiculo (Long id, String fabricante, String modelo, 
+			String anoFabricacao, String placa) {
+		
+		this.id = id;
+		this.fabricante = fabricante;
+		this.modelo = modelo;
+		this.anoFabricacao = anoFabricacao;
+		this.placa = placa;
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getAnoFabricacao() {
+		return anoFabricacao;
+	}
+
+	public void setAnoFabricacao(String anoFabricacao) {
+		this.anoFabricacao = anoFabricacao;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Veiculo other = (Veiculo) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Veiculo [id=" + id + ", fabricante=" + fabricante + ", modelo=" + modelo + ", anoFabricacao="
+				+ anoFabricacao + ", placa=" + placa + "]";
+	}
+
+}
---
+++ src/test/java/br/com/carcontrol/CarcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
